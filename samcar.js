adb1610602ac582a7793070a8daa976e6c8e0361
sohelmalek <sohelmalek@users.github.com>
2021-01-20 20:43:31 +0530

    new

---
+++ samcar.js
@@ -0,0 +1,142 @@
+/* ----dark light mode ON OFF SCRIPT---------------------------------------------- */
+
+const darkBtn = document.querySelector('.moon-icon');
+const bodyEl = document.querySelector('body');
+
+const darkMode = () => {
+    bodyEl.classList.toggle('dark')
+}
+
+darkBtn.addEventListener('click', () => {
+    // Get the value of the "dark" item from the local storage on every click
+    setDarkMode = localStorage.getItem('dark');
+
+    if (setDarkMode !== "on") {
+        darkMode();
+        // Set the value of the itwm to "on" when dark mode is on
+        setDarkMode = localStorage.setItem('dark', 'on');
+    } else {
+        darkMode();
+        // Set the value of the item to  "null" when dark mode if off
+        setDarkMode = localStorage.setItem('dark', null);
+    }
+});
+
+// Get the value of the "dark" item from the local storage
+let setDarkMode = localStorage.getItem('dark');
+
+// Check dark mode is on or off on page reload
+if (setDarkMode === 'on') {
+    darkMode();
+}
+/* ----dark light mode ON OFF SCRIPT END---------------------------------------------- */
+
+// ----------go to top -------------------------------------------------------------------------
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+// ----------go to top -------------------------------------------------------------------------
+
+// ----------------------navbar dropdown animation-------------------------------------------------------------------
+(function($) {
+    var defaults = {
+        sm: 540,
+        md: 720,
+        lg: 960,
+        xl: 1140,
+        navbar_expand: 'lg',
+        animation: true,
+        animateIn: 'fadeIn',
+
+        animation_duraion: '.3s',
+
+        animation_duraion: '3s',
+
+    };
+    $.fn.bootnavbar = function(options) {
+
+        var screen_width = $(document).width();
+        settings = $.extend(defaults, options);
+
+        if (screen_width >= settings.lg) {
+            $(this).find('.dropdown').hover(function() {
+                $(this).addClass('show');
+                $(this).find('.dropdown-menu').first().addClass('show');
+                if (settings.animation) {
+                    $(this).find('.dropdown-menu').first().addClass('animated ' + settings.animateIn);
+                }
+            }, function() {
+                $(this).removeClass('show');
+                $(this).find('.dropdown-menu').first().removeClass('show');
+            });
+        }
+
+        $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+            if (!$(this).next().hasClass('show')) {
+                $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+            }
+            var $subMenu = $(this).next(".dropdown-menu");
+            $subMenu.toggleClass('show');
+
+            $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+                $('.dropdown-submenu .show').removeClass("show");
+            });
+
+            return false;
+        });
+    };
+})(jQuery);
+
+// ---------navbar  scroll animation END-------------------------------------------------------------------
+$(document).ready(function() {
+    $(function() {
+
+        // $(".nav-item a").removeClass("text-white");
+        var navbar = $(".navbar")
+        $(window).on("scroll", function() {
+
+            if ($(window).scrollTop() > 50) {
+
+                navbar.addClass("h_70px");
+                navbar.removeClass("h_80px");
+
+                $(".main_nav").css('box-shadow', '0px 1px 20px #9099ff');
+                $(".navbar").css('padding', '1em 0em 1em 2em');
+
+
+
+
+            } else {
+
+
+                $(".main_nav").css('box-shadow', 'none');
+                navbar.removeClass("h_70px");
+                navbar.addClass("h_80px", 2000);
+                $(".navbar").css('padding', '1.8em 0em 1em 2em');
+
+
+
+
+            }
+
+        });
+    });
+
+});
+// <!-------------navbar menu scroll animation END---------------------------------------------->
